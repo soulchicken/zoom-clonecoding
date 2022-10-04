e8307a2dcd51b17eeb33cde1ccd0a558cfdbfd95
소울치킨 <soulfever01@naver.com>
2022-10-04 20:02:21 +0900

    step2 - 1 : 일단 무명으로 채팅이 가능해짐

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,41 @@
-alert("hi!");
+const messageList = document.querySelector("ul");
+const massageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+  console.log("New massage : ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server 🚫");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 5000);
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = massageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+};
+
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+};
+
+massageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -7,10 +9,30 @@ app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handlerListen = () =>
   console.log(`Listening on http:http://localhost:3000/`);
 
-app.listen(3000, handlerListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => {
+    console.log("Disconnected from Browser 🚫");
+  });
+
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => {
+      aSocket.send(message.toString("utf8"));
+    });
+  });
+  socket.send("hello!!!");
+});
+
+server.listen(3000, handlerListen);
